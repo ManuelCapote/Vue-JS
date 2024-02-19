8a88037382484c5fb05f9e1118d5816dbd358838
Manuel Capote <mcapote@nclcorp.com>
2024-02-19 12:20:42 -0500

    Two Way Data Binding

--- Vue-Fundamentals/app.js
+++ Vue-Fundamentals/app.js
@@ -1,8 +1,17 @@
-Vue.createApp({
-    data() {
-        return {
-            firstName: 'Jhon',
-            lastName: 'Doe'
-        }
-    }
-}).mount('#app')
+const vm = Vue.createApp({
+  data() {
+    return {
+      firstName: "Jhon",
+      lastName: "Doe",
+    };
+  },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
+}).mount("#app");
+
+// setTimeout(() => {
+//   vm.firstName = "Bob";
+// }, 2000);
