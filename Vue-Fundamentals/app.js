6399408e95b740982bc301a710c0c8dd7d4fda37
Manuel Capote <mcapote@nclcorp.com>
2024-02-20 11:27:34 -0500

    Directives and Passing Data with events

--- Vue-Fundamentals/app.js
+++ Vue-Fundamentals/app.js
@@ -3,15 +3,22 @@ const vm = Vue.createApp({
     return {
       firstName: "Jhon",
       lastName: "Doe",
+      url: "https://google.com",
+      age: 20,
     };
   },
   methods: {
     fullName() {
       return `${this.firstName} ${this.lastName}`;
     },
+    increment() {
+      return this.age++;
+    },
+    updateFirstName(event) {
+      this.firstName = event.target.value;
+    },
+    updateLastName(event) {
+      this.lastName = event.target.value;
+    },
   },
 }).mount("#app");
-
-// setTimeout(() => {
-//   vm.firstName = "Bob";
-// }, 2000);
